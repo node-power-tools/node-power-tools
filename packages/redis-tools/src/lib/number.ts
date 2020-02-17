8d1bf9f246f1476114cd1cd2d0a096c4a52d3575
James Dunnam <7660553+jimador@users.noreply.github.com>
2020-02-17 18:21:27 -0500

    adding github actions support and contribution docs (#1)
    * updating ignore and package namespace
    
    * adding function
    
    * adding .github template from create react app
    
    * npm publish action
    
    * updating publish.yml

--- packages/redis-tools/src/lib/number.ts
+++ packages/redis-tools/src/lib/number.ts
@@ -6,3 +6,7 @@ export function power(base: number, exponent: number): number {
   // This is a proposed es7 operator, which should be transpiled by Typescript
   return base ** exponent
 }
+
+export function subtract(lhs: number, rhs: number): number {
+  return lhs - rhs
+}
