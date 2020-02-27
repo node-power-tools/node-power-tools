5757ccafe923af97a22abafc7f1498f9050901f6
Brian Santarelli <brian.santarelli@somoglobal.com>
2020-02-26 20:43:58 -0500

    chore: Adding test coverage * Cleaned up some linting rules * Cherry picked bug fix from master onto this branch * Cherry picked target/lib config from master onto this branch

---
+++ packages/cache-tools/src/cache/redis/redis-cache.test.ts
@@ -0,0 +1,106 @@
+import { mock, mockClear, mockReset } from 'jest-mock-extended';
+import { buildPromise } from '../../../../../test/promise-util';
+import { DEFAULT_CACHE_CONFIGURATION } from '../cache';
+import { buildRegionPrefixedCacheKey, SimpleJsonCodec } from '../cache-codec';
+import { DEFAULT_CACHE_REGION_NAME, RedisCacheImpl } from './redis-cache';
+
+const mockRedisClient = mock<IHandyRedis>();
+const mockRedisLockFactory = mock<LockFactory>();
+const mockLogger = mock<NptLogger>();
+
+import { IHandyRedis } from 'handy-redis';
+import { LockFactory } from '@node-power-tools/concurrent-tools';
+import { NptLogger } from '@node-power-tools/logging-tools';
+
+describe('redis-cache tests', () => {
+  const tested = new RedisCacheImpl(mockRedisClient, mockRedisLockFactory, mockLogger);
+
+  const region = 'someRegion';
+  const key = 'someKey';
+  const errorMsg = 'Boom goes the dynamite!';
+
+  afterEach(() => {
+    mockReset(mockRedisClient);
+    mockClear(mockRedisClient);
+  });
+
+  describe('mergeCacheConfigWithDefault', () => {
+    it('empty partial', async () => {
+      const config = {};
+
+      const res = RedisCacheImpl.mergeCacheConfigWithDefault(config);
+
+      expect(res).toStrictEqual(DEFAULT_CACHE_CONFIGURATION);
+    });
+
+    it('partial', async () => {
+      const config = {
+        ttlSeconds: 20,
+        doubleCheckedPut: true,
+        codecId: 'specialCodec',
+      };
+
+      const res = RedisCacheImpl.mergeCacheConfigWithDefault(config);
+
+      expect(res.doubleCheckLockTtlSeconds).toStrictEqual(DEFAULT_CACHE_CONFIGURATION.doubleCheckLockTtlSeconds);
+      expect(res.ttlSeconds).toStrictEqual(config.ttlSeconds);
+      expect(res.doubleCheckedPut).toStrictEqual(config.doubleCheckedPut);
+      expect(res.codecId).toStrictEqual(config.codecId);
+    });
+  });
+
+  describe('get', () => {
+    const expectedKey = buildRegionPrefixedCacheKey(region, key);
+    const expectedDefaultRegionKey = buildRegionPrefixedCacheKey(DEFAULT_CACHE_REGION_NAME, key);
+    const expected = { some: '1', object: '2' };
+    const encodedRes = buildPromise(new SimpleJsonCodec().encode(expected));
+
+    it('happy path no data cached', async () => {
+      const res1 = await tested.get(key, region);
+
+      expect(mockRedisClient.get).toHaveBeenCalledWith(expectedKey);
+      expect(res1).toBeUndefined();
+    });
+
+    it('happy path data found', async () => {
+      mockRedisClient.get.calledWith(expectedKey).mockReturnValue(encodedRes);
+
+      const res2 = await tested.get(key, region);
+
+      expect(mockRedisClient.get).toHaveBeenCalledWith(expectedKey);
+      expect(res2).toEqual(expected);
+    });
+
+    it('happy path default region no cached value found', async () => {
+      const res = await tested.get(key);
+
+      expect(mockRedisClient.get).toHaveBeenCalledWith(expectedDefaultRegionKey);
+      expect(res).toBeUndefined();
+    });
+
+    it('happy path default region cached value found', async () => {
+      // FIXME: No idea why this mock won't return the return value.  The
+      //        expectation below proves the proper args are being used
+      // mockRedisClient.get.calledWith(expectedDefaultRegionKey).mockReturnValue(encodedRes)
+      mockRedisClient.get.mockReturnValue(encodedRes);
+
+      const res = await tested.get(key);
+
+      expect(mockRedisClient.get).toHaveBeenCalledWith(expectedDefaultRegionKey);
+      expect(res).toEqual(expected);
+    });
+
+    it('error', async () => {
+      mockRedisClient.get.mockImplementation(() => {
+        throw new Error(errorMsg);
+      });
+
+      try {
+        await tested.get(key, region);
+        fail('Should not get here');
+      } catch (e) {
+        expect(e.message).toEqual(expect.stringContaining(errorMsg));
+      }
+    });
+  });
+});
