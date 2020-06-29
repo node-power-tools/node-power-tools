282a96edb3cad849a060e50d736cfe9880c81d54
James Dunnam <7660553+jimador@users.noreply.github.com>
2020-06-29 17:18:02 -0400

    chore: ConsoleLogger tests

--- packages/logging-tools/src/logging/console-logger.ts
+++ packages/logging-tools/src/logging/console-logger.ts
@@ -6,45 +6,51 @@ import { NptLogger } from './logger'
  * A very basic console logger
  */
 export class ConsoleLogger implements NptLogger {
-  static readonly DT_FORMAT = DateTimeFormatter.ofPattern('MM-dd-yy HH:mm:ss.SSS')
+  private constructor(private readonly dateTimeFormatter: DateTimeFormatter) {}
 
-  public static newInstance(): ConsoleLogger {
-    return new ConsoleLogger()
+  /**
+   * Factory for {@code ConsoleLogger}
+   * @param dateFormat The date format for the logger.
+   */
+  public static newInstance(dateFormat = 'MM-dd-yy HH:mm:ss.SSS'): ConsoleLogger {
+    const dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormat)
+
+    return new ConsoleLogger(dateTimeFormatter)
   }
 
-  private handleLogMessage = (level: string, message: string, ...metadata: any[]): ConsoleLogger => {
-    if (metadata) {
-      console.log(
-        `${ConsoleLogger.DT_FORMAT.format(LocalDateTime.now())}: ${level}: ${message}: ${JSON.stringify(metadata)}`,
-      )
+  private handleLogMessage = (level: string, message: string, ...metadata: unknown[]): ConsoleLogger => {
+    const prefix = this.dateTimeFormatter.format(LocalDateTime.now())
+
+    if (metadata && metadata.length) {
+      console.log(`${prefix}: ${level}: ${message}: ${JSON.stringify(metadata)}`)
     } else {
-      console.log(`${ConsoleLogger.DT_FORMAT.format(LocalDateTime.now())}: ${level}: ${message}`)
+      console.log(`${prefix}: ${level}: ${message}`)
     }
 
     return this
   }
 
-  all(msg: string, ...metadata: any[]): ConsoleLogger {
-    return this.handleLogMessage('silly', msg, metadata)
+  all(msg: string, ...metadata: unknown[]): ConsoleLogger {
+    return this.handleLogMessage('silly', msg, ...metadata)
   }
 
-  debug(msg: string, ...metadata: any[]): ConsoleLogger {
-    return this.handleLogMessage('debug', msg, metadata)
+  debug(msg: string, ...metadata: unknown[]): ConsoleLogger {
+    return this.handleLogMessage('debug', msg, ...metadata)
   }
 
-  error(msg: string, ...metadata: any[]): ConsoleLogger {
-    return this.handleLogMessage('error', msg, metadata)
+  error(msg: string, ...metadata: unknown[]): ConsoleLogger {
+    return this.handleLogMessage('error', msg, ...metadata)
   }
 
-  info(msg: string, ...metadata: any[]): ConsoleLogger {
-    return this.handleLogMessage('info', msg, metadata)
+  info(msg: string, ...metadata: unknown[]): ConsoleLogger {
+    return this.handleLogMessage('info', msg, ...metadata)
   }
 
-  trace(msg: string, ...metadata: any[]): ConsoleLogger {
-    return this.handleLogMessage('trace', msg, metadata)
+  trace(msg: string, ...metadata: unknown[]): ConsoleLogger {
+    return this.handleLogMessage('trace', msg, ...metadata)
   }
 
-  warn(msg: string, ...metadata: any[]): ConsoleLogger {
-    return this.handleLogMessage('warn', msg, metadata)
+  warn(msg: string, ...metadata: unknown[]): ConsoleLogger {
+    return this.handleLogMessage('warn', msg, ...metadata)
   }
 }
