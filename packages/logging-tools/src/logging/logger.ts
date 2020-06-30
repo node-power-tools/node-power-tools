60cef4c38c7bb8a66c41be7cdea405602c48c38e
James Dunnam <7660553+jimador@users.noreply.github.com>
2020-06-30 12:44:15 -0400

    chore: generics cleanup

--- packages/logging-tools/src/logging/logger.ts
+++ packages/logging-tools/src/logging/logger.ts
@@ -1,18 +1,18 @@
 /**
  * Simple logging function
  */
-export interface NptLogFunctions<T extends NptLogger<T>> {
+export interface NptLogFunctions<T extends NptLogger> {
   (msg: string, ...metadata: unknown[]): T
 }
 
 /**
  * Logger wrapper interface
  */
-export interface NptLogger<T extends NptLogger<T>> {
-  all: NptLogFunctions<T>
-  debug: NptLogFunctions<T>
-  error: NptLogFunctions<T>
-  info: NptLogFunctions<T>
-  trace: NptLogFunctions<T>
-  warn: NptLogFunctions<T>
+export interface NptLogger {
+  all: NptLogFunctions<NptLogger>
+  debug: NptLogFunctions<NptLogger>
+  error: NptLogFunctions<NptLogger>
+  info: NptLogFunctions<NptLogger>
+  trace: NptLogFunctions<NptLogger>
+  warn: NptLogFunctions<NptLogger>
 }
